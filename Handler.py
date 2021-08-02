3f73fa381df9ad7ee4874ae5f71781bfd918554a
Lonely-Dark <vlad10game@gmail.com>
2021-08-02 19:52:06 +0300

    Create Handler.py

---
+++ Handler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# ---Lonely_Dark---
+# Python 3.9.6
+
+from Requests import Requests
+import logging
+
+# Create registrator with name main.handler
+module = logging.getLogger("main.handler")
